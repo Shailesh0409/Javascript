b42e1bc96a975fcbcfdd6c58591b2230af52a60c
Shailesh Agarmore <137064457+Shailesh0409@users.noreply.github.com>
2024-03-03 22:39:59 +0000

    Operations on Variables

--- Basics/03_operations.js
+++ Basics/03_operations.js
@@ -2,3 +2,9 @@ let str1 = "hello"
 let str2 = " Shailesh"
 let str3 = str1+str2
 console.log(str3); 
+
+// console.log(1 + "2");       // 12
+// console.log("1" + 2);       // 12
+// console.log("1" + 2 + 2);   // 122
+// console.log(1 + 3 + "2");   // 42
+// console.log((2 + 3) * 4 / 2); // 20
