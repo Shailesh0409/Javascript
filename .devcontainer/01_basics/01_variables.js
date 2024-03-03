f566d3060f0f7bc3cc16ac1c4c05350eb063fd5d
Shailesh Agarmore <137064457+Shailesh0409@users.noreply.github.com>
2024-03-04 04:12:44 +0530

    Update 01_variables.js

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -3,6 +3,6 @@ let accEmail = "Shailesh@123.com"
 var accCity = "Jaipur"
 
 accState = "MH"         // we can declare var like this but it is not recommandable at all
-let country             // if I run it without assign any value then it give udefined value (not null)
+let country             // if I run it without assign any value then it give udefined value (not null) 
 
 console.table([accId,accEmail,accCity,accState,country])
