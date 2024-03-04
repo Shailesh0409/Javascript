716a313a49aee1bbb14cd73fd0d1353b7c0ba27d
Shailesh Agarmore <137064457+Shailesh0409@users.noreply.github.com>
2024-03-04 09:46:36 +0000

    comparisons in js

---
+++ Basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2==2);
+// console.log(2<1);
+// console.log(2!=3);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);    // false
+// console.log(null == 0);   // false
+// console.log(null >= 0);   // true    here, it convert null into  number
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//*** Above all the comparison we mostly avoid*** */
+
+// In Javascript we mostly use the "===" operator
+
+
+console.log("2" == 2);   // true
+console.log("2" === 2);  // false => B'cause variaboles are diff. 
+// here it detect the diff of variables i.e string & number so output will be the "false"
+
